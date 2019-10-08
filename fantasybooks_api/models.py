afa60e08a685122042a03945845e246a1c2ece95
Camilo <camilolgon@gmail.com>
2019-10-08 18:16:44 -0300

    Defining the User model.

---
+++ fantasybooks_api/models.py
@@ -0,0 +1,16 @@
+from fantasybooks_api import db
+from datetime import datetime
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    name = db.Column(db.String(80), nullable=False)
+    surname = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    last_activity = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
