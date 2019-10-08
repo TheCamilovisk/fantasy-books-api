b328d7b1df12f147a834ceb52a865d03f7e5c0f1
Camilo <camilolgon@gmail.com>
2019-10-08 18:16:09 -0300

    Adding SQLAlchemy, Mashmallow and Migrate.

--- fantasybooks_api/__init__.py
+++ fantasybooks_api/__init__.py
@@ -1,15 +1,34 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_marshmallow import Marshmallow
+
+
+db = SQLAlchemy()
+ma = Marshmallow()
+migrate = Migrate()
+
+POSTGRES_URL = '127.0.0.1:5432'
+POSTGRES_USER = 'postgres'
+POSTGRES_PW = 'postgres'
+POSTGRES_DB = 'appwn_dev'
 
 
 def create_app(config=None):
-
     app = Flask(__name__)
+    app.config[
+        'SQLALCHEMY_DATABASE_URI'
+    ] = f'postgres+psycopg2://{POSTGRES_USER}:{POSTGRES_PW}@{POSTGRES_URL}/{POSTGRES_DB}'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     from .auth.resources import user_bp
 
     app.register_blueprint(user_bp)
 
     CORS(app)
+    db.init_app(app)
+    ma.init_app(app)
+    migrate.init_app(app, db)
 
     return app
