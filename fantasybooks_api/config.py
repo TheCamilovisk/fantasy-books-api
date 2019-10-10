e2a29ea27ecec206bce03f0bb6537bce7efce9aa
Camilo <camilolgon@gmail.com>
2019-10-10 16:07:14 -0300

    Adding JWT secret key.

--- fantasybooks_api/config.py
+++ fantasybooks_api/config.py
@@ -3,7 +3,8 @@ from os import getenv
 
 class BaseConfig:
     SECRET_KEY = 'dev'
-    
+    JWT_SECRET_KEY = 'dev'
+
     SQLALCHEMY_DATABASE_URI = 'postgres+psycopg2://{}:{}@{}/{}'.format(
         getenv("POSTGRES_USER"),
         getenv("POSTGRES_PW"),
