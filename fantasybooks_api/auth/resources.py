400b2f83ad1c373d1f1c3d25b227e64c5bd16b7c
Camilo Gonçalves <camilolgon@gmail.com>
2019-10-11 00:40:27 -0300

    Finishing token blacklisting.

--- fantasybooks_api/auth/resources.py
+++ fantasybooks_api/auth/resources.py
@@ -1,11 +1,10 @@
-from datetime import datetime
-
 from flask import Blueprint, current_app, request
 from flask_jwt_extended import (
     create_access_token,
     create_refresh_token,
     get_jti,
     get_jwt_identity,
+    get_raw_jwt,
     jwt_refresh_token_required,
     jwt_required,
 )
@@ -93,22 +92,18 @@ class LoginResource(Resource):
 
         from fantasybooks_api import redis
 
-        now = datetime.utcnow()
-
         access_token = create_access_token(identity=username)
-        access_then = now - current_app.config['JWT_ACCESS_TOKEN_EXPIRES']
         redis.set(
             get_jti(encoded_token=access_token),
             'false',
-            (now - access_then).seconds * 1.2,
+            current_app.config['JWT_ACCESS_TOKEN_EXPIRES'] * 1.2,
         )
 
         refresh_token = create_refresh_token(identity=username)
-        refresh_then = now - current_app.config['JWT_REFRESH_TOKEN_EXPIRES']
         redis.set(
             get_jti(encoded_token=refresh_token),
             'false',
-            (now - refresh_then).seconds * 1.2,
+            current_app.config['JWT_REFRESH_TOKEN_EXPIRES'] * 1.2,
         )
 
         return ({'access_token': access_token, 'refresh_token': refresh_token}, 200)
@@ -117,7 +112,41 @@ class LoginResource(Resource):
 class TokenRefreshResource(Resource):
     @jwt_refresh_token_required
     def post(self):
-        return {'access_token': create_access_token(identity=get_jwt_identity())}, 200
+        access_token = create_access_token(identity=get_jwt_identity())
+
+        from fantasybooks_api import redis
+
+        redis.set(
+            get_jti(encoded_token=access_token),
+            'false',
+            current_app.config['JWT_ACCESS_TOKEN_EXPIRES'] * 1.2,
+        )
+
+        return {'access_token': access_token}, 200
+
+
+class AccessTokenLogoutResource(Resource):
+    @jwt_required
+    def post(self):
+        jti = get_raw_jwt()['jti']
+
+        from fantasybooks_api import redis
+
+        redis.set(jti, 'true', current_app.config['JWT_ACCESS_TOKEN_EXPIRES'] * 1.2)
+
+        return {'msg': 'Access token revoked'}, 200
+
+
+class RefreshTokenLogoutResource(Resource):
+    @jwt_refresh_token_required
+    def post(self):
+        jti = get_raw_jwt()['jti']
+
+        from fantasybooks_api import redis
+
+        redis.set(jti, 'true', current_app.config['JWT_REFRESH_TOKEN_EXPIRES'] * 1.2)
+
+        return {'msg': 'Refresh token revoked'}, 200
 
 
 auth_bp = Blueprint('auth_bp', __name__)
@@ -125,3 +154,5 @@ auth_bp = Blueprint('auth_bp', __name__)
 auth_api = Api(auth_bp)
 auth_api.add_resource(LoginResource, '/login')
 auth_api.add_resource(TokenRefreshResource, '/login/refresh')
+auth_api.add_resource(AccessTokenLogoutResource, '/logout/access')
+auth_api.add_resource(RefreshTokenLogoutResource, '/logout/refresh')
