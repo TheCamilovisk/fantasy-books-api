14b44080ada1a0bd6007457eca6695afab0cea35
Camilo Gonçalves <camilolgon@gmail.com>
2019-10-11 00:40:55 -0300

    Using timedelta instead of relativedelta.

--- fantasybooks_api/config.py
+++ fantasybooks_api/config.py
@@ -1,6 +1,6 @@
 from os import getenv
 
-from dateutil import relativedelta
+from datetime import timedelta
 
 
 class BaseConfig:
@@ -19,7 +19,7 @@ class BaseConfig:
     JWT_SECRET_KEY = 'dev'
     JWT_BLACKLIST_ENABLED = True
     JWT_BLACKLIST_TOKEN_CHECKS = ('access', 'refresh')
-    JWT_ACCESS_TOKEN_EXPIRES = relativedelta(minutes=15)
-    JWT_REFRESH_TOKEN_EXPIRES = relativedelta(days=30)
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=15)
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
 
     REDIS_URL = 'redis://:redis@localhost:6379/0'
